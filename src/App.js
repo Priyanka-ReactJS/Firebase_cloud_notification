0fae658ec1b35392d7e89c662494a6d7aa684900
priyanka211992 <pppatel211992@gmail.com>
2021-01-11 12:42:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import firebase from './firebase';
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+  }
+  componentDidMount(){
+     const massaging = firebase.messaging()
+     massaging.requestPermission().then(()=>{
+       return massaging.getToken()
+     }).then(token=>{
+       console.log('Token : ',token)
+     }).catch((error) =>{
+      console.log('error',error)
+     })
+
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCB9uxYku_in0Ei_Myl4x3x6CHraFiVRVE",
+    authDomain: "notification-516c7.firebaseapp.com",
+    databaseURL: "https://notification-516c7.firebaseio.com",
+    projectId: "notification-516c7",
+    storageBucket: "notification-516c7.appspot.com",
+    messagingSenderId: "605800807548",
+    appId: "1:605800807548:web:428f4a8465e8ba2d4a1df9"
+}
+firebase.initializeApp(config)
+
+export default firebase
+  
